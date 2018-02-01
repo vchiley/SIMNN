329860302efc0b93cd54656c061e56dcf295c035
Vitaliy Chiley <vchiley@ucsd.edu>
2018-02-01 00:48:57 -0800

    compile into lib with init file

---
+++ simnn/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""
+SIMNN Library
+"""
+__author__ = 'Vitaliy Chiley'
+__date__ = '01/2018'
+
+from simnn.activations import Logistic_Sigmoid, Softmax, ReLU, Line, Identity
+from simnn.costs import CrossEntropy, BinaryCrossEntropy
+from simnn.layers import Linear
+from simnn.model import Model
+from simnn.utils import one_hot
